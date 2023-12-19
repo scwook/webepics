304ad0ca212b54bcc2dfaf51c8e5ea8f35a383b0
scwook <scwook@gmail.com>
2023-12-19 16:26:11 +0900

    menu test:

---
+++ widget.js
@@ -0,0 +1,5 @@
+const doughnutChartSVG = 
+'<svg width"174" height="174" style="fill:none"> \
+<circle class="type1-1-widget-background" cx="80" cy="80" r="80" /> \
+<circle class="type1-1-widget-foreground" id="test1" cx="80" cy="80" r="80" style="stroke-dasharray: 503; stroke-dashoffset: 503; background-color:green;" /> \
+</svg>'
